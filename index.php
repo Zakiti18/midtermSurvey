ad491f99f6d19a8881da678d3fa20fa5a4c6f841
Phillip Ball <pball@mail.greenriver.edu>
2021-05-14 16:05:40 -0400

    Added a survey page

--- index.php
+++ index.php
@@ -17,9 +17,30 @@ $f3 = Base::instance();
 
 // default route
 $f3->route('GET /', function (){
-    // display the home page
+    // display the page
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+// survey
+$f3->route('GET|POST /survey', function ($f3){
+    // the checkboxes
+    $userBoxes = array("This midterm is easy", "I like midterms", "Today is Monday");
+
+    // if the form has been submitted, add data to session and send user to summary
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // save the checked checkboxes in the session
+        $_SESSION['boxes'] = $_POST['boxes'];
+
+        header('location: summary');
+    }
+
+    // add the user data to the hive
+    $f3->set('userBoxes', $userBoxes);
+
+    // display the page
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 // run Fat-Free
